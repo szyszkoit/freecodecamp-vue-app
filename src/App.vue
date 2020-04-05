0d360f19ce7bf5560bce514564163c9f59993b2e
Krzysztof Szyszko <k.szyszko14@gmail.com>
2020-04-05 13:13:25 +0200

    disable submit button on init and on quesion change

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <Header />
+    <Header
+    :numCorrect = "numCorrect"
+    :numTotal = "numTotal"
+    />
     <b-container class="bv-example-row">
       <b-row>
         <b-col sm="6" offset="3">
@@ -8,6 +11,7 @@
           v-if="questions.length"
           :currentQuestion = questions[index]
           :next = "next"
+          :increment = "increment"
           />
         </b-col>
       </b-row>
@@ -28,13 +32,21 @@ export default {
   data() {
     return {
       questions: [],
-      index: 0
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0,
     }
   },
   methods: {
     next() {
       this.index++
     },
+    increment(isCorrect) {
+      if(isCorrect) {
+        this.numCorrect++
+      }
+      this.numTotal++
+    }
   },
   mounted() {
     fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -8,7 +8,7 @@
     <hr class="my-4">
     <b-list-group>
       <b-list-group-item
-        v-for = "(answer, index) in answers"
+        v-for = "(answer, index) in shuffledAnswers"
         :key="index"
         @click="selectAnswer(index)"
         :class="[selectedIndex === index ? 'selected' : '']"
@@ -17,26 +17,66 @@
       </b-list-group-item>
     </b-list-group>
 
-    <b-button variant="primary" href="#">Submit</b-button>
-    <b-button @click="next" variant="success" href="#">Next</b-button>
+    <b-button
+    @click="submitAnswer"
+    variant="primary"
+    :disabled="selectedIndex === null || answered">
+      Submit
+    </b-button>
+    <b-button
+    @click="next"
+    variant="success"
+    >
+      Next
+    </b-button>
   </b-jumbotron>
   </div>
 </template>
 <script>
+import _ from 'lodash';
   export default {
     props: {
       currentQuestion: Object,
-      next: Function
+      next: Function,
+      increment: Function
     },
     data() {
       return {
         selectedIndex: null,
+        correctIndex: null,
+        shuffledAnswers: [],
+        answered: false,
+      }
+    },
+    watch: {
+      currentQuestion: {
+        immediate: true,
+        handler() {
+          this.selectedIndex = null;
+          this.shuffleAnswers();
+          this.answered = false;
+        }
       }
     },
     methods: {
       selectAnswer(index) {
         this.selectedIndex = index;
         console.log(index);
+      },
+      submitAnswer() {
+        let isCorrect = false;
+        if(this.selectedIndex === this.correctIndex) {
+          isCorrect = true
+        }
+
+        this.answered = true
+
+        this.increment(isCorrect)
+      },
+      shuffleAnswers() {
+        let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+        this.shuffledAnswers = _.shuffle(answers)
+        this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
       },
     },
     computed: {
